f551f3894fe6e4698b6e74d5339d11252645510f
Cristian Marcel <lcristianiim@yahoo.com>
2016-01-30 01:00:20 +0200

    Test update

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ const caesarCrypter = require("../lib")
 
 tester.describe("Caesar cipher", test => {
     test.should("Encrypt", () => {
-        test.expect(caesarCrypter.encrypt(3,1,"abcdefgh","ab")).toBe(['d','e']);
+        test.expect(caesarCrypter.encrypt(3,1,"abcdefgh","ab")).toBe('de');
     }),
     test.should("Decrypt", () => {
-        test.expect(caesarCrypter.decrypt(3,1,"abcdefgh","de")).toBe(['a','b']);
+        test.expect(caesarCrypter.decrypt(3,1,"abcdefgh","de")).toBe('ab');
     });
 });
