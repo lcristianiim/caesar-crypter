74149f6393fee512c44fa0d85d84c2dd3c4de72b
Cristian Marcel <lcristianiim@yahoo.com>
2016-01-30 00:24:37 +0200

    Example output show

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 const caesarCrypter = require("../lib");
 
 // Encrypt the 'ab' message using base string 'abcdefgh', step 1, direction -1
-console.log(caesarCrypter.encrypt(1,-1,"abcdefgh","ab"));
+console.log(caesarCrypter.encrypt(1,-1,"abcdefgh","ab")); // [ 'h', 'a' ]
 // Decrypt the 'ha' message using base string 'abcdefgh', step 1, direction -1
-console.log(caesarCrypter.decrypt(1,-1,"abcdefgh","ha"));
+console.log(caesarCrypter.decrypt(1,-1,"abcdefgh","ha")); // [ 'a', 'b' ]
